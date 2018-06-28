432a6239072388f3406d8cd769e54968f35bf527
lvix <iamliufu@gmail.com>
2018-06-28 13:28:03 +0800

    modified the init script

--- scripts/initialization.py
+++ scripts/initialization.py
@@ -1,29 +1,33 @@
-import re 
+
+import re
 from rupa.models import * 
 
-print('Now creating the Admin user:')
-print('username: admin')
-while True:
-    email = input('input email:')
-    if re.compile(r'^[\w]+@[\w]+\.[\w]+$').match(email):
-        break
 
-while True:
-    password = input('input password:')
-    if re.compile(r'^[\w,<>;:\-_=&%#@!~`\*\.\?\+\$\^\[\]\(\)\{\}\|\\\/]{8,16}$').match(password):
-        break 
-    else:
-        print('should be 8~16 characters, includes numbers, letters and symbols')
+def init_rupa():
+    print('Now creating the Admin user:')
+    print('username: admin')
+    while True:
+        email = input('input email:')
+        if re.compile(r'^[\w]+@[\w]+\.[\w]+$').match(email):
+            break
 
-# 创建 admin
-admin = User(username='admin',
+    while True:
+        password = input('input password:')
+        if re.compile(r'^[\w,<>;:\-_=&%#@!~`\*\.\?\+\$\^\[\]\(\)\{\}\|\\\/]{8,16}$').match(password):
+            break
+        else:
+            print('should be 8~16 characters, includes numbers, letters and symbols')
+
+    # 创建 admin
+    admin = User(username='admin',
                 nickname='admin',
                 email=email,
                 password=password,
                 role=User.ROLE_ADMIN,
                 )
 
-db.session.add(admin)
-db.session.commit()
+    db.session.add(admin)
+    db.session.commit()
+    print('Initialized rupa successfully')
 
 
